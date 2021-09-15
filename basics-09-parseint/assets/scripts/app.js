ff9453512d56c13209639fe2541c2cb0d757316c
HishamRaihan <mr.hraihan@gmail.com>
2021-09-15 15:29:22 -0400

    first commit

--- basics-09-parseint/assets/scripts/app.js
+++ basics-09-parseint/assets/scripts/app.js
@@ -1,9 +1,50 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+//
+function getUserNumberInput() {
+	return parseInt(userInput.value);
+}
+function createAndWriteOutPut(operator, reslutBeforeCalc, calcNumber) {
+	// out put text
+	const calcDescription = `${reslutBeforeCalc} ${operator} ${calcNumber}`;
+	outputResult(currentResult, calcDescription);
+}
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, '');
+	// to avoid repeating code stored the function into another variable that we can use all over our code
+	const enteredNumber = getUserNumberInput();
+	// wrap the template literal will be converted to a string, building description before the code
+	//const calcDescription = `${currentResult} + ${enteredNumber}`;
+	// storing initial result with current result before we change it
+	const initialResult = currentResult;
+	// parseInt converts the text into number
+	currentResult = currentResult + enteredNumber;
+	createAndWriteOutPut('+', initialResult, enteredNumber);
+}
+// logic will be similar to the add function
+function subract() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult - enteredNumber;
+	createAndWriteOutPut('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult - enteredNumber;
+	createAndWriteOutPut('*', initialResult, enteredNumber);
+}
+
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult = currentResult - enteredNumber;
+	createAndWriteOutPut('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
